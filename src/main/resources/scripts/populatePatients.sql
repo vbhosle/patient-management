28041853c7510c50f123fa4b7aee432531858fec
Viraj Bhosle <bhosleviraj@gmail.com>
2018-10-16 19:44:09 +0530

    Add SQL scripts

---
+++ src/main/resources/scripts/populatePatients.sql
@@ -0,0 +1,5 @@
+USE PATIENTDB;
+insert into patient values(1, 'Sam Dmello',STR_TO_DATE('02-05-1980', '%m-%d-%Y'));
+insert into patient values(2, 'Max Escobar',STR_TO_DATE('12-06-1986', '%m-%d-%Y'));
+insert into patient values(3, 'Samantha Rodrigues',STR_TO_DATE('10-02-1989', '%m-%d-%Y'));
+insert into patient values(4, 'Vanita Lobbo',STR_TO_DATE('09-07-1990', '%m-%d-%Y'));
