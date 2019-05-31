6dd381ee4f053a7dd1ed44b0d1f1ab5461b0ce1a
viraj bhosle <viraj.bhosle@citiustech.com>
2019-05-31 19:00:49 +0530

    Close context

--- src/main/java/com/patient/app/AppDemo.java
+++ src/main/java/com/patient/app/AppDemo.java
@@ -3,23 +3,23 @@ package com.patient.app;
 import java.util.Date;
 
 import org.h2.tools.Server;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.patient.entities.Patient;
 import com.patient.services.PatientServiceImpl;
 
 public class AppDemo {
-	
+
 	public static void main(String args[]) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		//get the h2 console url
-		Server server = context.getBean("h2WebServer", Server.class);
-		System.out.println(server.getURL());
-		PatientServiceImpl service = context.getBean("patientServiceImpl", PatientServiceImpl.class);
-		Patient patient = new Patient();
-		patient.setName("Test");
-		patient.setDob(new Date());
-		service.createPatient(patient);
+		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");) {
+			// get the h2 console url
+			Server server = context.getBean("h2WebServer", Server.class);
+			System.out.println(server.getURL());
+			PatientServiceImpl service = context.getBean("patientServiceImpl", PatientServiceImpl.class);
+			Patient patient = new Patient();
+			patient.setName("Test");
+			patient.setDob(new Date());
+			service.createPatient(patient);
+		}
 	}
 }
